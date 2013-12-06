164a155fefa87aa5f5b8f70ba40ce9955d53f5f0
Rori Kurniadi <rori.kurniadi@yahoo.com>
2013-12-06 16:58:27 +0530

    Update using_chillitalk_lang.php

--- wp-content/themes/new_chillitalk/application/language/english/using_chillitalk_lang.php
+++ wp-content/themes/new_chillitalk/application/language/english/using_chillitalk_lang.php
@@ -12,15 +12,15 @@ $lang['using_chillitalk.text1'] =  ascii_to_entities("
 <h3>
 What are Chillitalk Local Numbers?</h3>
 <p>Chillitalk Local Numbers are phone numbers assigned to each international contact you add in your 
-<a href='http://chillitalk.co.uk/en/login_member/'>Chillitalk account</a>. 
+<a href='http://chillitalk.ca/en/login_member/'>Chillitalk account</a>. 
 Each Local Number is 'local' to you. With Local Numbers you call internationally for the price of a local call.  
 </p>
 <h3>How do I use Chillitalk Local Numbers? </h3> 
-<p>You need to <a href='http://chillitalk.co.uk/en/sign_up'>register</a> your landline or mobile phone with Chillitalk. You can add up to 50 international contact numbers in your account. For every international contact number you add, you will be assigned a unique Local Number. You can then call your contacts by dialling the Local Number from any of your registered phones. </p>
+<p>You need to <a href='http://chillitalk.ca/en/sign_up'>register</a> your landline or mobile phone with Chillitalk. You can add up to 50 international contact numbers in your account. For every international contact number you add, you will be assigned a unique Local Number. You can then call your contacts by dialling the Local Number from any of your registered phones. </p>
   
 <h3>Why should I use Local Numbers?</h3> 
 <p>
-Local Numbers are a great way to make <a href='http://chillitalk.co.uk/en/rates/'>cheap international calls</a> from your landline or mobile phone.
+Local Numbers are a great way to make <a href='http://chillitalk.ca/en/rates/'>cheap international calls</a> from your landline or mobile phone.
 You can save up to 50 Local Numbers and use them whenever you want. Local Numbers let you make cheap
 international calls easily. No Smartphone, Apps or internet needed. 
 </p>
@@ -30,7 +30,7 @@ international calls easily. No Smartphone, Apps or internet needed.
 <h3>Can I use my Local Numbers abroad? </h3> 
 <p>Yes, but remember that Local Numbers can only be used in the country they were created for.
 If you change country, you need different Local Numbers. Go to 
-<a href='http://chillitalk.co.uk/en/login_member/'>My Account</a> and Chillitalk will give you new numbers for your new location. You can only use Local Numbers from your registered phone so you may need a SIM card from the new country you are in to avoid roaming charges.</p> 	
+<a href='http://chillitalk.ca/en/login_member/'>My Account</a> and Chillitalk will give you new numbers for your new location. You can only use Local Numbers from your registered phone so you may need a SIM card from the new country you are in to avoid roaming charges.</p> 	
 <h3>Can I get Local Numbers for every country in the world?</h3> 
-<p>No. Please see our <a href='http://chillitalk.co.uk/en/chillitalk_access_number/'>list of countries</a> for which you can get Local Numbers.</p>");
+<p>No. Please see our <a href='http://chillitalk.ca/en/chillitalk_access_number/'>list of countries</a> for which you can get Local Numbers.</p>");
  
