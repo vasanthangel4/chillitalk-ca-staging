91ed6c35f8cda0403ec283eac721738bca794979
Rori Kurniadi <rori.kurniadi@yahoo.com>
2013-12-06 16:24:36 +0530

    Update topup_lang.php

--- wp-content/themes/new_chillitalk/application/language/english/myaccount/topup_lang.php
+++ wp-content/themes/new_chillitalk/application/language/english/myaccount/topup_lang.php
@@ -15,8 +15,9 @@ $lang['topup.third']=  ascii_to_entities("<h3>
 Top Up with E-Voucher 
 </h3> 
                    <div class='rightcunt'>
-                   <p class='commfrt txtas'>You can top up using an E-voucher. E-vouchers are available from any retailer that displays the logos below. Buy your E-voucher and enter the E-voucher PIN code 
-                   <a class='ttxtblue' href='http://chillitalk.co.uk/en/myaccount/top_up/voucher'>Here</a>
+                   <p class='commfrt txtas'>You can top up using an E-voucher. E-vouchers are available from any retailer 
+                   that displays the logos below. Buy your E-voucher and enter the E-voucher PIN code 
+                   <a class='ttxtblue' href='http://chillitalk-ca-staging.azurewebsites.net/en/myaccount/top_up/voucher'>Here</a>
            
           
                    </p>  "); 
