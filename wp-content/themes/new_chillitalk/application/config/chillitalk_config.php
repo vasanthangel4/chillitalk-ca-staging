9744a20657c4b4ca2d27b89a8cae1317b2f1b15c
rorikurniadi <rori.kurniadi@yahoo.com>
2013-12-09 19:55:32 +0700

    add config

---
+++ wp-content/themes/new_chillitalk/application/config/chillitalk_config.php
@@ -0,0 +1,45 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your CodeIgniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|	http://example.com/
+|
+| If this is not set then CodeIgniter will guess the protocol, domain and
+| path to your installation.
+|
+*/
+$config['country_name_web'] = 'CANADA';
+
+$config['country_code_web'] = 'CAN';
+
+$config['country_code2_web'] = 'CA';
+
+$config['rates_web'] = '&cent;/min';
+
+$config['call_rates_web'] = '&cent;/min';
+
+$config['sms_rates_web'] = '&cent;/SMS';
+
+$config['app_code_web'] = 'CTPCA';
+
+$config['prefix_phone_web'] ='1';
+
+$config['currency_web'] = 'CAD';
+
+$config['currency_symbol_web'] = '$';
+
+$config['site_code_web'] = 'CA1';
+
+$config['plan_id_web'] = 152;
+
+$config['user_flag'] = get_template_directory_uri().'/img/chillitalkusers-ca.jpg';
+
+$config['small_flag'] = 'caflg';
+
+$config['big_flag'] = get_template_directory_uri().'/img/sign_up_flags/ca.png';
