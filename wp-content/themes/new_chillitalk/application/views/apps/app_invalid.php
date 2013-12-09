0c3d3747acf891f190ad15f309b69cc4574e470e
rorikurniadi <rori.kurniadi@yahoo.com>
2013-12-09 20:12:29 +0700

    add prefix 2

--- wp-content/themes/new_chillitalk/application/views/apps/app_invalid.php
+++ wp-content/themes/new_chillitalk/application/views/apps/app_invalid.php
@@ -75,14 +75,14 @@ Get started with Chillitalk
                           <input type="text" class="<?php echo $this->config->item('small_flag'); ?>" value="+<?php echo $this->config->item('prefix_phone_web'); ?>" name="UK" readonly="readonly">
                           <?php if(form_error('phone_number')) { ?>
                           
-                          <input type="text" name="phone_number" style="color:red;border-color:red" required="required" pattern="[0-9]+" />
+                          <input type="text" name="phone_number" style="color:red;border-color:red" required="required" pattern="^[+0-9]{5,16}$" />
                           
                           <input type="hidden" name="device" id="device"/>
                           <?php echo form_error('phone_number','<span style="color:red">','</span>'); ?>
                           
 						  <?php }else{ ?>
                           
-                          <input type="text" name="phone_number" required="required" pattern="[0-9]+" />
+                          <input type="text" name="phone_number" required="required" pattern="^[+0-9]{5,16}$" />
                           <input type="hidden" name="device" id="device" value="android"/>
                           
 						  <?php } ?>
@@ -128,14 +128,14 @@ Get started with Chillitalk
                           <input type="text" class="<?php echo $this->config->item('small_flag'); ?>" value="+<?php echo $this->config->item('prefix_phone_web'); ?>" name="UK" readonly="readonly">
                           <?php if(form_error('phone_number')) { ?>
                           
-                          <input type="text" name="phone_number" style="color:red;border-color:red" required="required" pattern="[0-9]+" />
+                          <input type="text" name="phone_number" style="color:red;border-color:red" required="required" pattern="^[+0-9]{5,16}$" />
                           
                           <input type="hidden" name="device" id="device"/>
                           <?php echo form_error('phone_number','<span style="color:red">','</span>'); ?>
                           
 						  <?php }else{ ?>
                           
-                          <input type="text" name="phone_number" required="required" pattern="[0-9]+" />
+                          <input type="text" name="phone_number" required="required" pattern="^[+0-9]{5,16}$" />
                           <input type="hidden" name="device" id="device" value="iphone"/>
                           
 						  <?php } ?>
